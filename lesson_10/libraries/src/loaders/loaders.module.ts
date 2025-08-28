0836defdb25c7533965525c95e47d7d0642a30a3
Dean Walston <deanahwalston01@gmail.com>
2025-08-28 14:55:19 +0000

    feat: add DeanWalstonLoader implementation and integrate into loaders module

--- lesson_10/libraries/src/loaders/loaders.module.ts
+++ lesson_10/libraries/src/loaders/loaders.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AnthonyMaysLoader } from './anthony_mays_loader.js';
+import { DeanWalstonLoader } from './dean_walston_loader.js';
 
 export const Loaders = Symbol.for('Loaders');
 
 const LOADER_PROVIDERS = [
   AnthonyMaysLoader,
+  DeanWalstonLoader
 ];
 
 @Module({
