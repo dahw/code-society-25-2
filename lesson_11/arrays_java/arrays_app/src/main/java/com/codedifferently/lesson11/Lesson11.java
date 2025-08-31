fa536edbcfbf179ca14d63ce144a76734e2e1816
Dean Walston <deanahwalston01@gmail.com>
2025-08-31 00:01:54 +0000

    feat: Dean Lesson_11 implement solutions for LeetCode functions in Java and TypeScript

--- lesson_11/arrays_java/arrays_app/src/main/java/com/codedifferently/lesson11/Lesson11.java
+++ lesson_11/arrays_java/arrays_app/src/main/java/com/codedifferently/lesson11/Lesson11.java
@@ -1,5 +1,6 @@
 package com.codedifferently.lesson11;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Lesson11 {
@@ -9,7 +10,13 @@ public class Lesson11 {
    * https://leetcode.com/problems/concatenation-of-array
    */
   public int[] getConcatenation(int[] nums) {
-    return null;
+    int n = nums.length;
+    int[] result = new int[n * 2];
+    for (int i = 0; i < n; i++) {
+      result[i] = nums[i];
+      result[i + n] = nums[i];
+    }
+    return result;
   }
 
   /**
@@ -17,6 +24,13 @@ public class Lesson11 {
    * https://leetcode.com/problems/find-words-containing-character/
    */
   public List<Integer> findWordsContaining(String[] words, char x) {
-    return null;
+    int n = words.length;
+    List<Integer> result = new ArrayList<>();
+    for (int i = 0; i < n; i++) {
+      if (words[i].indexOf(x) != -1) {
+        result.add(i);
+      }
+    }
+    return result;
   }
 }
--- lesson_11/arrays_ts/src/lesson11.ts
+++ lesson_11/arrays_ts/src/lesson11.ts
@@ -3,7 +3,13 @@
  * https://leetcode.com/problems/concatenation-of-array
  */
 export function getConcatenation(nums: number[]): number[] {
-  return [];
+  const n = nums.length;
+  const result = new Array(n * 2);
+  for (let i = 0; i < n; i++) {
+    result[i] = nums[i];
+    result[i + n] = nums[i];
+  }
+  return result;
 }
 
 /**
@@ -11,5 +17,12 @@ export function getConcatenation(nums: number[]): number[] {
  * https://leetcode.com/problems/find-words-containing-character/
  */
 export function findWordsContaining(words: string[], x: string): number[] {
-  return [];
+  const result: number[] = [];
+  const n = words.length;
+  for (let i = 0; i < n; i++) {
+    if (words[i].indexOf(x) !== -1) {
+      result.push(i);
+    }
+  }
+  return result;
 }
