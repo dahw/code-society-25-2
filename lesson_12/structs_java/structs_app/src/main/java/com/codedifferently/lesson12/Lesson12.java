600d9053e98c34fca38119ef9510a1de370246c3
Dean Walston <deanahwalston01@gmail.com>
2025-09-02 17:24:56 +0000

    feat: Dean add gameResult methods in Lesson12 and complete stack method

--- lesson_12/structs_java/structs_app/src/main/java/com/codedifferently/lesson12/Lesson12.java
+++ lesson_12/structs_java/structs_app/src/main/java/com/codedifferently/lesson12/Lesson12.java
@@ -7,6 +7,35 @@ public class Lesson12 {
    * https://github.com/yang-su2000/Leetcode-algorithm-practice/tree/master/3062-winner-of-the-linked-list-game
    */
   public String gameResult(ListNode head) {
-    return null;
+    if (head == null) {
+      return "Tie";
+    }
+
+    int evenWins = 0;
+    int oddWins = 0;
+    ListNode current = head;
+
+    // Process pairs of nodes (Even team vs Odd team)
+    while (current != null && current.next != null) {
+      // Apply game logic based on current.val
+      int evenValue = current.val;
+      int oddValue = current.next.val;
+
+      if (evenValue > oddValue) {
+        evenWins++;
+      } else if (oddValue > evenValue) {
+        oddWins++;
+      }
+      // If equal, no one wins that round
+      current = current.next.next; // Move to next pair
+    }
+
+    if (evenWins > oddWins) {
+      return "Even";
+    } else if (oddWins > evenWins) {
+      return "Odd";
+    } else {
+      return "Tie";
+    }
   }
 }
