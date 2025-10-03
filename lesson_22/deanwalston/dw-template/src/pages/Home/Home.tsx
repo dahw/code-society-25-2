8082d4c5291f391652d3c1648d9b4e75b6dd9b08
Dean Walston <deanahwalston01@gmail.com>
2025-10-03 19:12:30 +0000

    feat: Redesign Home page as landing page
    - Remove ProgramList from Home page to create clean landing page
    - Add CTA buttons: 'View Our Programs' and 'Add a Program'
    - Style buttons with hover effects and proper spacing
    - Implement navigation to Programs and AddProgram pages

--- lesson_22/deanwalston/dw-template/src/pages/Home/Home.tsx
+++ lesson_22/deanwalston/dw-template/src/pages/Home/Home.tsx
@@ -1,5 +1,6 @@
 import './Home.scss';
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 export const Home: React.FC = () => {
   return (
@@ -17,58 +18,15 @@ export const Home: React.FC = () => {
             technical and cognitive skills they need to excel in
             technology-driven workplaces.
           </div>
+          <div className="hero-actions">
+            <Link to="/programs" className="cta-button">
+              View Our Programs
+            </Link>
+            <Link to="/add-program" className="cta-button secondary">
+              Add a Program
+            </Link>
+          </div>
         </div>
-      </section>
-      <section className="programs-section">
-        <h2>
-          Our <em className="highlight">Programs</em>
-        </h2>
-        <ul className="programs">
-          <li className="program">
-            <h3>Swine Short Loin</h3>
-            <p>
-              Swine short loin burgdoggen ball tip, shank ham hock bacon.
-              Picanha strip steak pork, swine boudin ham meatball meatloaf
-              leberkas sausage. Turkey beef andouille kielbasa rump pastrami
-              biltong chislic alcatra buffalo prosciutto jowl. Pastrami chicken
-              sirloin swine capicola landjaeger jowl boudin pork chop shankle
-              bresaola turducken leberkas ham.
-            </p>
-          </li>
-          <li className="program">
-            <h3>Bacon Ipsum</h3>
-            <p>
-              Bacon ipsum dolor amet leberkas chuck biltong pork loin sirloin
-              bresaola rump frankfurter. Shoulder doner strip steak chuck. Short
-              ribs venison salami chuck sirloin jowl chislic cupim swine cow.
-              Corned beef chuck frankfurter tenderloin venison biltong andouille
-              leberkas kielbasa sausage t-bone turducken fatback. Pork picanha
-              buffalo bacon tail salami meatball, jowl chislic.
-            </p>
-          </li>
-          <li className="program">
-            <h3>Picanha Swine Jowl</h3>
-            <p>
-              Picanha swine jowl meatball boudin pastrami bresaola fatback
-              shankle pork belly cow. Frankfurter ground round shank corned beef
-              chuck. Jerky frankfurter fatback capicola hamburger, pork
-              prosciutto bresaola ham porchetta rump t-bone pancetta tenderloin.
-              Fatback ham hock prosciutto, tenderloin shoulder salami tri-tip
-              leberkas bacon turducken chislic venison sausage frankfurter.
-            </p>
-          </li>
-          <li className="program">
-            <h3>Kevin Chicken T-Bone</h3>
-            <p>
-              Kevin chicken t-bone spare ribs shankle bacon drumstick kielbasa
-              cow jowl doner salami chuck andouille. Rump spare ribs bresaola
-              frankfurter shankle. Bresaola ribeye turducken, cow leberkas
-              boudin biltong sirloin filet mignon ham pork belly shank.
-              Tenderloin sirloin pancetta pork loin shankle venison turducken
-              boudin. Brisket tenderloin salami ham bresaola burgdoggen.
-            </p>
-          </li>
-        </ul>
       </section>
     </article>
   );
